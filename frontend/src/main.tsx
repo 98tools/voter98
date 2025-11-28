3fd210195ffe0a8a250f479e3c957866b2e5fee5
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-11-28 20:16:27 +0300

    feat: add color configuration and favicon setup from environment variables

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { injectColors , setFavicon } from './config/colors'
+import { injectColors , setFavicon } from './config/startup'
 
 // Inject colors and site title from environment variables
 injectColors()
